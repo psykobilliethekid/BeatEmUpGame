9293e43ca0ed1bcebf9075527ccbd5f2c1dfd831
Danielle Moss <dani.j.moss@gmail.com>
2019-06-12 17:10:57 -0400

    started player movement script and created tag manager script

---
+++ 3DBeatEmUp/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //private PlayerAnimation player_Animation;
+    private Rigidbody myBody;
+
+    public float walk_Speed = 3f;
+    public float z_Speed = 1.5f;
+
+    private float rotation_Y = -90f;
+    private float rotation_Speed = 15f;
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+        //player_Animation = GetComponentInChildren<PlayerAnimation>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DetectMovement()
+    {
+        myBody.velocity = new Vector3(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) * (-walk_Speed),
+            myBody.velocity.y,
+            Input.GetAxisRaw(Axis.VERTICAL_AXIS) * (-z_Speed));
+    }
+
+
+} //class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
