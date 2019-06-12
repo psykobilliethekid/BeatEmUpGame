8dcdc6364a4e4fd85153aa934a2b81f7e67bf354
Danielle Moss <dani.j.moss@gmail.com>
2019-06-12 18:44:12 -0400

    started player walking script

---
+++ 3DBeatEmUp/Assets/Scripts/PlayerScripts/CharacterAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+   public void Walk (bool move)
+    {
+        //50:59
+    }
+
+}// class
