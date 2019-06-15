3ccd80d320b122824ae0938c0e7d06fa35a4f323
Danielle Moss <dani.j.moss@gmail.com>
2019-06-15 18:30:02 -0400

    Started setting up combo attack

--- 3DBeatEmUp/Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ 3DBeatEmUp/Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -2,9 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Available animations for combos
+public enum ComboState
+{
+    NONE, 
+    PUNCH_1,
+    PUNCH_2, 
+    PUNCH_3, 
+    KICK_1, 
+    KICK_2
+}
+
 public class PlayerAttack : MonoBehaviour
 {
     private CharacterAnimation player_Anim;
+
+    private bool activateTimerToReset;
+
+    private float default_Combo_Timer = 0.4f;
+    private float current_Combo_Timer;
+
+    private ComboState current_Combo_State;
 
     void Awake()
     {
@@ -14,7 +32,9 @@ public class PlayerAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Setting default combo timer and current combo state
+        current_Combo_Timer = default_Combo_Timer;
+        current_Combo_State = ComboState.NONE;
     }
 
     // Update is called once per frame
@@ -24,11 +44,15 @@ public class PlayerAttack : MonoBehaviour
         ComboAttacks();
     }
 
+
+    // Combo Attacks
     void ComboAttacks()
     {
         if(Input.GetKeyDown(KeyCode.Z))
         {
             player_Anim.Punch_1();
+
+            // 1:14:18
         }
 
         if(Input.GetKeyDown(KeyCode.X))
