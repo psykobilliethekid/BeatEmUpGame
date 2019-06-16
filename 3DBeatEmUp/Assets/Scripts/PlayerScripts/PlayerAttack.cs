3e4d2c0b02bade8feec97ddf595b0f5b1efc098c
Danielle Moss <dani.j.moss@gmail.com>
2019-06-16 19:09:49 -0400

    Completed set up for attack combos

--- 3DBeatEmUp/Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ 3DBeatEmUp/Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -42,6 +42,9 @@ public class PlayerAttack : MonoBehaviour
     {
         // Play combo attacks
         ComboAttacks();
+
+        // Allow repeat button presses for combos
+        ResetComboState();
     }
 
 
@@ -51,6 +54,14 @@ public class PlayerAttack : MonoBehaviour
         // Punch
         if(Input.GetKeyDown(KeyCode.Z))
         {
+
+            // Continue the combo if it's punch_3, exit and 
+            // go to X key if kick_1 or kick_2 is pressed
+            if (current_Combo_State == ComboState.PUNCH_3 ||
+                current_Combo_State == ComboState.KICK_1 ||
+                current_Combo_State == ComboState.KICK_2)
+                return;
+
             current_Combo_State++;
             activateTimerToReset = true;
             current_Combo_Timer = default_Combo_Timer;
@@ -68,8 +79,6 @@ public class PlayerAttack : MonoBehaviour
             if (current_Combo_State == ComboState.PUNCH_3)
             {
                 player_Anim.Punch_3();
-
-                //1:16:13
             }
 
 
@@ -78,7 +87,61 @@ public class PlayerAttack : MonoBehaviour
         // Kick
         if(Input.GetKeyDown(KeyCode.X))
         {
-            player_Anim.Kick_1();
+            // Exit of kick_2 or punch_3 is pressed because there
+            // are no longer any combos to perform
+            if (current_Combo_State == ComboState.KICK_2 ||
+                current_Combo_State == ComboState.PUNCH_3)
+                return;
+
+
+            // If you've pressed nothing, punch_1 or punch_2
+            // go to kick_1 to chain the combo
+            if (current_Combo_State == ComboState.NONE ||
+                current_Combo_State == ComboState.PUNCH_1 ||
+                current_Combo_State == ComboState.PUNCH_2)
+            {
+                current_Combo_State = ComboState.KICK_1;
+            }
+
+            // If kick_1 is pressed continue the combo
+            else if (current_Combo_State == ComboState.KICK_1)
+            {
+                // Move on to kick_2
+                current_Combo_State++;
+            }
+
+            activateTimerToReset = true;
+            current_Combo_Timer = default_Combo_Timer;
+
+
+            // Play the kick animation
+            if (current_Combo_State == ComboState.KICK_1)
+            {
+                player_Anim.Kick_1();
+            }
+
+            if (current_Combo_State == ComboState.KICK_2)
+            {
+                player_Anim.Kick_2();
+            }
+        }
+    }
+
+    // Allows player to press punch or kick repeatedly
+    void ResetComboState()
+    {
+        if(activateTimerToReset)
+        {
+            current_Combo_Timer -= Time.deltaTime;
+
+            // Can now chain the combo once again
+            if(current_Combo_Timer <= 0f)
+            {
+                current_Combo_State = ComboState.NONE;
+
+                activateTimerToReset = false;
+                current_Combo_Timer = default_Combo_Timer;
+            }
         }
     }
 
