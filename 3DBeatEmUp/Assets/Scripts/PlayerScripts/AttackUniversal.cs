bd326b57ce54b2052e60b03d3d8e46744ce47fe3
Danielle Moss <dani.j.moss@gmail.com>
2019-06-19 18:21:31 -0400

    Started to set up the hit detection for the player and the enemy

---
+++ 3DBeatEmUp/Assets/Scripts/PlayerScripts/AttackUniversal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackUniversal : MonoBehaviour
+{
+    public LayerMask collisionLayer;
+    public float radius = 1f;
+    public float damage = 2f;
+
+    public bool is_Player, is_Enemy;
+
+    public GameObject hit_FX;
+
+
+    void Update()
+    {
+        DetectCollision();
+    }
+
+
+    // Detects if the Player or Enemy hits
+    void DetectCollision()
+    {
+        Collider[] hit = Physics.OverlapSphere(transform.position, radius, collisionLayer);
+    }
+} //class
